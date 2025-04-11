050480511106f950ad25e4965ca8d4b482527e5a
Michael S. Taylor <mtaylor@semo.edu>
2025-04-11 08:07:37 -0500

    All analysis and plots for water depth.

--- water_depth.R
+++ water_depth.R
@@ -1,31 +1,11 @@
+# All analysis and plots for water depth.
+# Run 01_read_data.R first
 library(tidyverse)
 library(janitor)
 library(RColorBrewer)
 
-section_colors <- brewer.pal(3, "Set1")
 
-water_depth <- read_csv(
-  "data/water_depth.csv",
-  skip = 1,
-  na = "N/A",
-  col_names = c(
-    "N",
-    "section",
-    "box_id",
-    "min",
-    "max",
-    "mode"
-  )
-)
-
-water_depth <- water_depth |> 
-  mutate(section =
-           ifelse(section == "E", "Pool2",
-                  ifelse(section == "N", "Pool1", "Pool3")))
-
-# First, strip the < and > symbols and look at the values.
-
-x <- water_depth %>% mutate(across(4:6, ~as.numeric(gsub("<|>", "", .x))))
+# Wrangle -----------------------------------------------------------------
 
 box_id_levels <- c(
   "S48",
@@ -60,55 +40,53 @@ box_id_levels <- c(
   "N2"
 )
 
-xx <- x |> 
+# Leave original water_depth data alone, just in case.
+wd <- water_depth |> 
   mutate(box_id = factor(box_id, levels = box_id_levels))
 
-xx |> group_by(section) |> 
-  summarise(mn = mean(mode))
+wd |> group_by(pool) |> 
+  summarise(mn = mean(depth_mode))
 
 
+# Analysis ----------------------------------------------------------------
 
-xx |> group_by(section) |> 
+wd_aov <- aov(depth_mode ~ pool, data = wd)
+summary(wd_aov)
+
+# Significant difference so determine which are diff.
+wd_hsd <- TukeyHSD(wd_aov)
+
+wd_hsd_tbl <- tidy(wd_hsd)
+
+wd_hsd_tbl
+
+# Plots -------------------------------------------------------------------
+
+# Data distribution plot
+water_depth_plot <- wd |> group_by(pool) |> 
   ggplot() +
   geom_segment(
     aes(
       x = box_id,
-      y = min,
+      y = depth_min,
       xend = box_id,
-      yend = max,
-      color = section
+      yend = depth_max,
+      color = pool
     )
   ) +
-  # geom_point(
-  #   aes(
-  #     x = box_id,
-  #     y = min,
-  #     colour = section
-  #   ),
-  #   size = 1
-  # ) +
-  # geom_point(
-  #   aes(
-  #     x = box_id,
-  #     y = max,
-  #     colour = section
-  #   ),
-  #   size = 1
-  # ) +
   geom_point(
     aes(
       x = box_id,
-      y = mode,
-      color = section
+      y = depth_mode,
+      color = pool
     ),
-#    shape = 0,
     size = 2
   ) +
   coord_flip() +
   labs(
     y = "Water depth (m)",
     x = "Nest Box",
-    color = "Section"
+    color = NULL
     ) +
   theme_minimal() +
   scale_color_brewer(
@@ -117,9 +95,9 @@ xx |> group_by(section) |>
   ) +
   theme(text = element_text(family = "Linux Libertine O"))
 
-
 ggsave(
   file = "water_depth_plot.png",
+  plot = water_depth_plot,
   width = 1600,
   height = 1600,
   units = "px",
@@ -128,45 +106,34 @@ ggsave(
 )
 
 
+# Tukey HSD plot for appenxix
 
+wd_hsd_plot <- wd_hsd_tbl |>
+  ggplot() +
+  geom_vline(
+    xintercept = 0,
+    color = "grey",
+    linewidth = 0.5,
+    linetype = 2
+  ) +
+  geom_segment(aes(x = conf.low, y = contrast, xend = conf.high)) +
+  geom_point(aes(x = estimate, y = contrast), size = 2) +
+  scale_x_continuous(limits = c(-2, 2), breaks = c(-2, -1, 0, 1, 2)) +
+  labs(x = "Differences in mean depth of pools", y = "Comparison") +
+  theme_minimal() +
+  theme(text = element_text(family = "Linux Libertine O")) +
+  theme(panel.grid = element_blank())
 
-# x |> 
-#   ggplot() +
-#   geom_point(
-#     aes(section, min),
-#     fill = "white",
-#     position = position_jitter(width = 0.3),
-#     shape = 25,
-#     size = 2) +
-#   # geom_point(
-#   #   aes(section, mode), 
-#   #   fill = "gray50",
-#   #   position = position_jitter(width = 0.3),
-#   #   pch = 22,
-#   #   size = 2) +
-#   geom_point(
-#     aes(section, max), 
-#     fill = "black",
-#     position = position_jitter(width = 0.3),
-#     shape = 24,
-#     size = 2) +
-#   labs(
-#     x = "Section",
-#     y = "Water width (m)"
-#     ) +
-#   theme_minimal()
-# 
-# x |> group_by(section) |> 
-#   ggplot() +
-#   geom_point(
-#     aes(
-#       x = min, 
-#       y = max, 
-#       color = section
-#     ),
-#     size = 2
-#   )
-# 
-# 
+wd_hsd_plot
+
+ggsave(
+  filename = "hsd_wd_plot.png",
+  plot = wd_hsd_plot,
+  width = 1600,
+  height = 900,
+  dpi = 300,
+  units = "px",
+  bg = "white"
+)
 
 
