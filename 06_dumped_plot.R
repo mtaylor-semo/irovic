8c5e35ff8faae72c57a639ac5b4dbea6afee959c
Michael S. Taylor <mtaylor@semo.edu>
2025-04-11 11:45:16 -0500

    Added number to file name for analysis order. Some code clean up.

--- 06_dumped_plot.R
+++ 06_dumped_plot.R
@@ -1,34 +1,11 @@
+# Plot the dumped data
+# Run 01_read_data.R first
 library(tidyverse)
-library(janitor)
-
-dumped <- read_csv(
-  "data/glm_data.csv",
-  skip = 1,
-  col_names = c(
-    "box_id",
-    "section",
-    "attempt",
-    "spp_code",
-    "orig",
-    "dump_nesting",
-    "dumped_num",
-    "hatched_num",
-    "depth_mode",
-    "width_mode",
-    "coverage"
-  )
-)
 
 dumped1 <- dumped |>
   mutate(box = str_extract(box_id, "[0-9]+")) |>
-  relocate(box, .after = section) |>
-  mutate(species = if_else(spp_code == "M", "merg", "wood")) |>
-  mutate(
-    section = if_else(
-      section == "E", "Pool2", if_else(
-        section == "N", "Pool1", "Pool3"
-      )
-    )) |> 
+  relocate(box, .after = pool) |>
+  mutate(species = if_else(spp_code == "M", "merg", "wood")) |> 
   select(-c(
     "spp_code",
     "dump_nesting",
@@ -38,22 +15,20 @@ dumped1 <- dumped |>
   )) |>
   filter(dumped_num > 0)
 
-section <- c("Pool2", "Pool2", "Pool2", "Pool2", "Pool1", "Pool1", "Pool3", "Pool3", "Pool3", "Pool3", "Pool3")
+pool <- c("Pool2", "Pool2", "Pool2", "Pool2", "Pool1", "Pool1", "Pool3", "Pool3", "Pool3", "Pool3", "Pool3")
 dumped_num <- c(1, 3, 4, 5, 2, 4, 2, 3, 4, 6, 10)
 box <- c("7/65", "62", "22", "62", "93", "70", "14/44", "7", "20", "20", "7")
 species <- c("wood", "wood", "wood", "wood", "merg", "wood", "wood", "wood", "wood", "wood", "merg")
 
 
-dlabs <- tibble(section, dumped_num, box, species)
+# Separate tibble to make labels of box numbers that are used
+# in the final plot.
+dlabs <- tibble(pool, dumped_num, box, species)
 
 dumped_plot <- dumped1 |>
-  group_by(section) |>
+  group_by(pool) |>
   ggplot(aes(shape = species, color = species)) +
-#  geom_text(aes(x = section, y = dumped_num, label = box),
-#            nudge_x = 0.12) +
-  geom_point(data = dumped1, aes(x = section, y = dumped_num), size = 2) +
-  #    width = 0.20,
-  #    height = 0) +
+  geom_point(data = dumped1, aes(x = pool, y = dumped_num), size = 2) +
   scale_y_continuous(breaks = c(2, 4, 6, 8 , 10)) +
   xlab(NULL) +
   ylab("Number of dumped eggs") +
@@ -73,7 +48,7 @@ dumped_plot <- dumped1 |>
 dumped_plot <- dumped_plot +
   geom_text(
     data = dlabs,
-    aes(x = section, y = dumped_num, label = box),
+    aes(x = pool, y = dumped_num, label = box),
     nudge_x = 0.25,
     color = "black",
     show.legend = FALSE,
